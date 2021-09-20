1f9e2b0921461f854adaa5ae4531e5393287f3d9
Unmesh Padhye <unmeshpadhye@gmail.com>
2021-09-20 16:58:04 -0700

    2nd commit

--- src/pages/PageAbout.vue
+++ src/pages/PageAbout.vue
@@ -2,7 +2,7 @@
   <q-page class="q-pa-lg">
     <h4 class="q-mt-none q-mb-md text-weight-bold">About Twitter</h4>
     <div class="text-body1">
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint voluptatem optio dolore quisquam molestiae temporibus     nesciunt nam laudantium in alias, aspernatur eos? Magnam obcaecati nihil provident molestiae, corrupti nam minus?
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.  nothing Sint voluptatem optio dolore quisquam molestiae temporibus     nesciunt nam laudantium in alias, aspernatur eos? Magnam obcaecati nihil provident molestiae, corrupti nam minus?
       </p>
       <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint voluptatem optio dolore quisquam molestiae temporibus     nesciunt nam laudantium in alias, aspernatur eos? Magnam obcaecati nihil provident molestiae, corrupti nam minus?
       </p>
