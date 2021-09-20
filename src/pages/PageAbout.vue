8f947efb20223007e0463a81ece5017e5906fe1b
Unmesh Padhye <unmeshpadhye@gmail.com>
2021-09-20 16:42:35 -0700

    init

---
+++ src/pages/PageAbout.vue
@@ -0,0 +1,19 @@
+<template>
+  <q-page class="q-pa-lg">
+    <h4 class="q-mt-none q-mb-md text-weight-bold">About Twitter</h4>
+    <div class="text-body1">
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint voluptatem optio dolore quisquam molestiae temporibus     nesciunt nam laudantium in alias, aspernatur eos? Magnam obcaecati nihil provident molestiae, corrupti nam minus?
+      </p>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint voluptatem optio dolore quisquam molestiae temporibus     nesciunt nam laudantium in alias, aspernatur eos? Magnam obcaecati nihil provident molestiae, corrupti nam minus?
+      </p>
+    </div>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'PageAbout'
+})
+</script>
---
+++ src/pages/PageHome.vue
@@ -0,0 +1,57 @@
+<template>
+  <q-page>
+    <div class="q-py-lg q-px-md row items-end
+    q-col-gutter-md">
+      <div class="col">
+        <q-input
+          bottom-slots
+          v-model="newTweetContent"
+          class="new-tweet"
+          autogrow
+          placeholder="What's happening?"
+          counter
+          maxlength="280"
+        >
+            <template v-slot:before>
+              <q-avatar size="xl">
+                <img src="https://kb.rspca.org.au/wp-content/uploads/2018/11/golder-retriever-puppy.jpeg">
+              </q-avatar>
+            </template>
+          </q-input>
+      </div>
+      <div class="col col-shrink">
+        <q-btn
+              :disable="!newTweetContent"
+                unelevated
+                rounded
+                no-caps
+                class="q-mb-lg"
+                color="primary"
+                label="Tweet"
+          />
+      </div>
+    </div>
+
+    <q-separator/>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'PageHome',
+  data () {
+    return {
+      newTweetContent: ''
+    }
+  }
+})
+</script>
+
+<style lang="sass">
+.new-tweet
+  textarea
+    font-size: 19px
+    line-height: 1.4 !important
+</style>
